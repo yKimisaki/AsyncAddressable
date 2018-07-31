2d74482374031bc097397c88a7b81231a9b9403a
Yoshitaka Kimisaki <y.kimisaki@gmail.com>
2018-07-31 11:44:53 +0900

    Unityちゃんを表示してみる

---
+++ AsyncAddressable/Assets/Sample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using UniRx.Async;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Sample : MonoBehaviour
+{
+    async Task Start()
+    {
+        var logger = UnityEngine.Debug.unityLogger;
+        try
+        {
+            logger.Log("Start: " + Time.realtimeSinceStartup.ToString());
+
+            var unitychan = LazyUniTask.LoadAddressables<GameObject>("Assets/UnityChan/Prefabs/unitychan.prefab");
+        
+            logger.Log("Created task: " + Time.realtimeSinceStartup.ToString());
+
+            var go = Instantiate(await unitychan);
+            go.transform.position = new Vector3(Random.Range(-10f, 10f), 0f, 0f);
+
+            logger.Log("First instantiate: " + Time.realtimeSinceStartup.ToString());
+
+            var go2 = Instantiate(await unitychan);
+            go2.transform.position = new Vector3(Random.Range(-10f, 10f), 0f, 0f);
+
+            logger.Log("Second instantiate: " + Time.realtimeSinceStartup.ToString());
+        }
+        catch (AggregateException ae)
+        {
+            foreach (var e in ae.InnerExceptions)
+                logger.LogException(e);
+        }
+        catch (Exception e)
+        {
+            logger.LogException(e);
+        }
+    }
+}
---
+++ AsyncAddressable/Assets/UniRx.Async/LazyUniTask.cs
@@ -0,0 +1,54 @@
+﻿#if CSHARP_7_OR_LATER
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement;
+
+namespace UniRx.Async
+{
+    public static class LazyUniTask
+    {
+        public static UniTask<T> Create<T>(Func<UniTask<T>> taskFactory) where T : class
+        {
+            return new UniTask<T>(taskFactory);
+        }
+
+        // Resources.LoadAsync
+        public static UniTask<T> LoadResources<T>(string path) where T : UnityEngine.Object
+        {
+            return new UniTask<T>(() =>
+            {
+                var completionSource = new UniTaskCompletionSource<T>();
+                var loadTask = Resources.LoadAsync<T>(path);
+                loadTask.completed += x => completionSource.TrySetResult(loadTask.asset as T);
+                return completionSource.Task;
+            });
+        }
+
+        // Addressable Assets System
+        public static UniTask<T> LoadAddressables<T>(IResourceLocation location) where T : class
+        {
+            return new UniTask<T>(() =>
+            {
+                var completionSource = new UniTaskCompletionSource<T>();
+                Addressables.LoadAsset<T>(location).Completed += x => completionSource.TrySetResult(x.Result);
+                return completionSource.Task;
+            });
+        }
+
+        // Addressable Assets System
+        public static UniTask<T> LoadAddressables<T>(object key) where T : class
+        {
+            return new UniTask<T>(() =>
+            {
+                var completionSource = new UniTaskCompletionSource<T>();
+                Addressables.LoadAsset<T>(key).Completed += x => completionSource.TrySetResult(x.Result);
+                return completionSource.Task;
+            });
+        }
+    }
+}
+
+#endif
